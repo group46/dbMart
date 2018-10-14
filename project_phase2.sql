e978196f9a2bf50304e04be253a8137bd28f01c9
Gina <1ginahong@gmail.com>
2018-10-14 12:47:42 -0700

    First commit

---
+++ project_phase2.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS 'Craigsbook';
+USE 'Craigsbook';
+
+DROP TABLE IF EXISTS 'Advertisement';
+DROP TABLE IF EXISTS 'Post_Likes';
+DROP TABLE IF EXISTS 'User';
+DROP TABLE IF EXISTS 'Seller';
+DROP TABLE IF EXISTS 'Buyer';
+DROP TABLE IF EXISTS 'Transaction_Has';
+DROP TABLE IF EXISTS 'Comment';
+DROP TABLE IF EXISTS 'Product_Posts';
+DROP TABLE IF EXISTS 'Product_Photo';
+DROP TABLE IF EXISTS 'Uses';
+
+CREATE TABLE 'Advertisement' (
+    'adID'    
+)
