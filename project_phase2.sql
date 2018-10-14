8b08f962c6b07dc714b6f348c19c2fac9c8ea96e
Gina <1ginahong@gmail.com>
2018-10-14 13:14:12 -0700

    advertisement, post0likes table created

--- project_phase2.sql
+++ project_phase2.sql
@@ -1,21 +1,31 @@
 CREATE DATABASE IF NOT EXISTS 'Craigsbook';
 USE 'Craigsbook';
 
-DROP TABLE IF EXISTS 'Advertisement';
-DROP TABLE IF EXISTS 'Post_Likes';
-DROP TABLE IF EXISTS 'User';
-DROP TABLE IF EXISTS 'Seller';
-DROP TABLE IF EXISTS 'Buyer';
-DROP TABLE IF EXISTS 'Transaction_Has';
-DROP TABLE IF EXISTS 'Comment';
-DROP TABLE IF EXISTS 'Product_Posts';
-DROP TABLE IF EXISTS 'Product_Photo';
-DROP TABLE IF EXISTS 'Uses';
+DROP TABLE IF EXISTS 'advertisement';
+DROP TABLE IF EXISTS 'post_likes';
+DROP TABLE IF EXISTS 'user';
+DROP TABLE IF EXISTS 'seller';
+DROP TABLE IF EXISTS 'buyer';
+DROP TABLE IF EXISTS 'transaction_has';
+DROP TABLE IF EXISTS 'comment';
+DROP TABLE IF EXISTS 'product_posts';
+DROP TABLE IF EXISTS 'product_photo';
+DROP TABLE IF EXISTS 'uses';
 
-CREATE TABLE 'Advertisement' (
+CREATE TABLE 'advertisement' (
     'aid' INTEGER,
     'adimage' VARCHAR(500), -- image LINK
     'adlink' VARCHAR(500), -- if click on add, should link to
     'adtag' VARCHAR(20),
     PRIMARY KEY (aid)
 );
+
+CREATE TABLE 'post_likes' (
+    'likes_uid' VARCHAR(30),
+    'likes_postid' INTEGER,
+    'uid' VARCHAR(30),
+    'postid' INTEGER,
+    PRIMARY KEY (likes_uid, likes_postid),
+    FOREIGN KEY (uid) REFERENCES user,
+    FOREIGN KEY (postid) REFERENCES product_posts
+)
