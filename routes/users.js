f0b3b3f47a78157fdd76e4ea362a23ffa7040597
Gina <1ginahong@gmail.com>
2018-11-14 23:32:08 -0800

    work on add account button on login. What happens after account added still incomplete. needs more work

--- routes/users.js
+++ routes/users.js
@@ -15,70 +15,64 @@ module.exports = {
     },
 
     getBuyers: (req, res) => {
-      let query = "SELECT uid, first_name, last_name, DATE_FORMAT(`birthdate`, '%Y-%m-%d') AS birthdate FROM `buyer` ORDER BY first_name DESC"
-      db.query(query, (err, result) => {
-        if (err) {
-          res.redirect('/')
-        }
-        res.render('users.ejs', {
-          title: "DBMart | Buyers"
-          , users: result
-        })
-      })
+        let query = "SELECT uid, first_name, last_name, DATE_FORMAT(`birthdate`, '%Y-%m-%d') AS birthdate FROM `buyer` ORDER BY first_name DESC"
+
+        db.query(query, (err, result) => {
+            if (err) {
+                res.redirect('/')
+            }
+            res.render('users.ejs', {
+                title: "DBMart | Buyers"
+                ,users: result
+            });
+        });
     },
 
     getSellers: (req, res) => {
-      let query = "SELECT uid, first_name, last_name, DATE_FORMAT(`birthdate`, '%Y-%m-%d') AS birthdate FROM `seller`"
+        let query = "SELECT uid, first_name, last_name, DATE_FORMAT(`birthdate`, '%Y-%m-%d') AS birthdate FROM `seller`"
 
-      db.query(query, (err, result) => {
-        if (err) {
-          res.redirect('/')
-        }
-        res.render('users.ejs', {
-          title: "DBMart | Sellers"
-          , users: result
-        })
-      })
+        db.query(query, (err, result) => {
+            if (err) {
+                res.redirect('/')
+            }
+            res.render('users.ejs', {
+                title: "DBMart | Sellers"
+                ,users: result
+            });
+        });
     },
 
-    insertBuyer: (req, res) => {
+    insertUser: (req, res) => {
+        let uid = req.body.user_id
+        let first_name = req.body.first_name
+        let last_name = req.body.last_name
+        let password = req.body.password
+        let birthdate = req.body.birthdate
 
-      let uid = req.body.uid
-      let first_name = req.body.first_name
-      let last_name = req.body.last_name
-      let password = req.body.password
-      let birthdate = req.body.birthdate
+        let nameQuery = "SELECT * FROM `user` WHERE uid = '" + uid + "'"
+        let query = "INSERT INTO `user` VALUES ('" + uid + "', '" + first_name + "', '" + last_name + "', '" + password + "', '" + birthdate + "')"
 
-      let nameQuery = "SELECT * FROM 'user' AS u WHERE u.uid = '" + uid + "'"
-      let query = "INSERT INTO 'buyer' (uid, first_name, last_name, birthdate) values ('" + uid + "', '" + first_name + "', '" + last_name + "', '" + password + "', '" + birthdate + "')"
-
-      db.query(nameQuery,(err, res) => {
-        if (err) {
-          throw err
-          console.log(err)
-        }
-        //if userid is empty, no user exists, redirects to make new acc
-        if (res.length = 0) {
-                res.redirect('/add-acc')
-        } else {
-          //if error, throw random invalid entry, else add user to buyers
-          db.query(query, (err, res) => {
+        db.query(nameQuery,(err, res1) => {
             if (err) {
-              throw err
-              message = "Invalid entry";
-              res.render('productpost.ejs', {
-                message,
-                title: "DBMart | Make new post"
-              })
+                message = "Error"
+                console.log(err);
             } else {
-            //log insertion, no extra render needed?
-            console.log(res)
-            //res.redirect('/makepost-buy')
-          }
-        })
-      }
-    })
-  },
+                //if error, throw random invalid entry, else add user to buyers
+                db.query(query, (err, res2) => {
+                    if (err) {
+                        message = "Invalid entry";
+                    } else {
+                        //log insertion, no extra render needed?
+                        console.log(res2);
+                        res.render('users.ejs', {
+                            title: "DBMart | User Added"
+                            ,users: res1
+                        });
+                    }
+                });
+            }
+        });
+    },
 
     insertSeller: (req, res) => {
 
@@ -88,12 +82,11 @@ module.exports = {
       let password = req.body.password
       let birthdate = req.body.birthdate
 
-      let nameQuery = "SELECT * FROM 'user' AS u WHERE u.uid = '" + uid + "'"
-      let query = "INSERT INTO 'seller' (uid, first_name, last_name, birthdate) values ('" + uid + "', '" + first_name + "', '" + last_name + "', '" + password + "', '" + birthdate + "')"
+      let nameQuery = "SELECT * FROM `user` AS u WHERE u.uid = '" + uid + "'"
+      let query = "INSERT INTO `seller` (uid, first_name, last_name, birthdate) values ('" + uid + "', '" + first_name + "', '" + last_name + "', '" + password + "', '" + birthdate + "')"
 
       db.query(nameQuery,(err, res) => {
         if (err) {
-          throw err
           //print invalid entry?
           console.log(err)
         }
