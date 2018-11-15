23442537e78d4e36ca40cf75e19ed0a5dd0b997e
theblackathena <nicholaschin20@gmail.com>
2018-11-14 18:06:02 -0800

    partial change, merge with master

--- routes/addpost.js
+++ routes/addpost.js
@@ -4,7 +4,6 @@ module.exports = {
 
 
   getAddPostPage: (req, res) => {
-    res.send("add post page")
     res.render('add-post.ejs', {
       title: "DBMart | Create New Post",
       message: '',
@@ -17,31 +16,47 @@ module.exports = {
       console.log('line 13 reached');
     // }
 
-    let postid = "SELECT COUNT(*) + 1 FROM 'product_posts'";
-    let postby = "SELECT uid FROM product_posts";
-    // let prodname = '';
-    // let proddescription = '';
-    // let date = '';
-    // let price = '';
-    // let sold = '';
+    let qpostid = "SELECT COUNT(*) + 1 FROM 'product_posts'";
+    let postbyid = req.body.postbyid;
+    let product_name = req.body.product_name;
+    let product_description = req.body.product_description;
+    let post_date = req.body.post_date;
+    let price = req.body.price;
+    let sold = 0;
     // let uploadedFile = req.files.image;
     // let image_name = uploadedFile.name;
     // let fileExtension = uploadedFile.mimetype.split('/')[1];
     // image_name = postby + '.' + fileExtension;
 
+    // conditional queries
+    let qpostby = "SELECT uid FROM user WHERE uid = '" + postbyid + "'";
 
-    // WHY WOULD YOU NEED THIS?
-    // db.query(postid, (err, result) => {
-    //   if (err) {
-    //     return res.status(500).send(err);
-    //   }
-    //   if (result) {
-    //     message = 'Post added';
-    //     res.render('', {
-    //       message,
-    //       title: "DBMart | Post Created"
-    //     });
-    //   }
-    // });
+
+    db.query(qpostby, (err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+        console.log("error man");
+      }
+
+// If no such user, redirect to Login page (after Alex pushes)
+      // if (result.value == NULL) {
+      //   message = 'User does not exist. Create a new account.'
+      //   res.render('add-acc.ejs' , {
+      //     message,
+      //     title: ""
+      //   });
+      // }
+
+      else {
+        // everything should work fine
+        let postquery = "INSERT INTO product_posts (postid, uid, product_description, product_name, post_date, price, sold) VALUES ('qpostid',  'postbyid' , 'product_name' , 'product_description', 'post_date', 'price', 'sold')";
+        db.query(postquery, (err, result) => {
+          if (err) {
+            return res.status(500).send(err);
+          }
+          res.redirect('/');
+        });
+      }
+    });
+  }
 }
-};
